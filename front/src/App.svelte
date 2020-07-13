8b50d62ceb8793ba3a59ea0b7049037db002ad1e
gz <resourceldg@gmail.com>
2020-07-13 20:14:18 -0300

    Change sapper(immature) for svelte template

---
+++ front/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import client from "./graphql-client";
+  import gql from "graphql-tag";
+  let getUsers = {
+    username: "",
+    email: ""
+  };
+
+  const query = gql`
+    query {
+      getUsers {
+        username
+        email
+        id
+      }
+    }
+  `;
+
+  const getUser = client.subscribe({
+    query
+  });
+</script>
+
+<main>
+  {#if $getUser && $getUser.data}
+    {#each $getUser.data.getUsers as u}
+      <tr>
+        <td>{u.username}</td>
+        <td>{u.email}</td>
+        <td>{u.id}</td>
+      </tr>
+    {/each}
+  {/if}
+</main>
